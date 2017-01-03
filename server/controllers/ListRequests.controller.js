4db390182bd5365f1d6f35eacaa02bb96241c3a4
Jirat Onaree <jirat.onaree@gmail.com>
2017-01-03 19:12:39 +0700

    Add request type

--- server/controllers/ListRequests.controller.js
+++ server/controllers/ListRequests.controller.js
@@ -12,7 +12,7 @@ import { tempPassword } from '../../secret_config.json';
 export function getAllListRequests(req, res) {
   ListRequests.find({ is_approved: false })
     .sort('-created_at')
-    .select('_id company_name title salary exp skills allow_remote company_image ')
+    .select('_id request_type company_name title salary exp skills allow_remote company_image ')
     .exec((err, requests) => {
       if (err) {
         res.status(500).send(err);
@@ -60,66 +60,71 @@ export function insertListRequest(req, res) {
 
   if (!title || !tags || !company_name || !country || !city || !location_detail || !email || !password) {
     res.status(403).end();
-  }
+  } else {
+    // Save company profile
+    const newCompany = new Companies({
+      company_image, company_name,
+      company_email: email,
+      company_location: { country, city, detail: location_detail },
+      allow_remote: remote_check,
+      password,
+    });
 
-  // Save company profile
-  const newCompany = new Companies({
-    company_image, company_name,
-    company_email: email,
-    company_location: { country, city, detail: location_detail },
-    allow_remote: remote_check,
-    password,
-  });
-
-  let company_id = '';
-  newCompany.save((err1, saved1) => {
-    if (err1) {
-      res.status(500).send(err1);
-    } else {
-      company_id = saved1._id;
-
-      let min = 0;
-      let max = 0;
-      if (exp_condition === 'between') {
-        min = exp_between_min;
-        max = exp_between_max;
-      } else if (exp_condition === 'more_than') {
-        min = exp_more_than;
-        max = 99;
+    let company_id = '';
+    newCompany.save((err1, saved1) => {
+      if (err1) {
+        console.log('err1');
+        res.status(500).send(err1);
       } else {
-        min = 0;
-        max = 99;
-      }
+        console.log('pass err1');
+        company_id = saved1._id;
 
-      const skills = tags.map((skill) => skill.text);
-
-      const newListRequest = new ListRequests({
-        company_id, company_image, company_name,
-        company_email: email,
-        company_location: { country, city, detail: location_detail },
-        password,
-        allow_remote: remote_check,
-        skills,
-        title,
-        exp: { condition: exp_condition, min, max, has_intern: intern_check },
-        salary: {
-          min: salary_min || 0,
-          max: salary_max || 9999999,
-        },
-        details,
-        how_to_apply,
-        additional_note,
-      });
-
-      newListRequest.save((err2, saved2) => {
-        if (err2) {
-          res.status(500).send(err2);
+        let min = 0;
+        let max = 0;
+        if (exp_condition === 'between') {
+          min = exp_between_min;
+          max = exp_between_max;
+        } else if (exp_condition === 'more_than') {
+          min = exp_more_than;
+          max = 99;
         } else {
-          res.json({ list_request_id: saved2._id });
+          min = 0;
+          max = 99;
         }
-      });
-    }
-  });
+
+        const skills = tags.map((skill) => skill.text);
+
+        const newListRequest = new ListRequests({
+          request_type: 'new',
+          company_id, company_image, company_name,
+          company_email: email,
+          company_location: { country, city, detail: location_detail },
+          password,
+          allow_remote: remote_check,
+          skills,
+          title,
+          exp: { condition: exp_condition, min, max, has_intern: intern_check },
+          salary: {
+            min: salary_min || 0,
+            max: salary_max || 9999999,
+          },
+          details,
+          how_to_apply,
+          additional_note,
+        });
+
+        newListRequest.save((err2, saved2) => {
+          if (err2) {
+            console.log(err2);
+            res.status(500).send(err2);
+          } else {
+            console.log('pass err2');
+            res.json({ list_request_id: saved2._id });
+          }
+        });
+      }
+    });
+  }
 }
 
 /**
@@ -150,12 +155,69 @@ export function checkListRequest(req, res) {
 }
 
 /**
- * Check list request
- * @param req
- * @param res
- * @returns void
+ * Approve new List request
  */
-export function approveListRequest(req, res) {
+export function approveNewListRequest(req, res) {
+  const { list_request_id } = req.params;
+  const { password } = req.body;
+  if (password !== tempPassword) {
+    res.json({
+      ok: false,
+      msg: 'whoa calm down mann. can\'t we be cool?',
+    });
+  } else {
+    ListRequests.findOne(
+      { _id: list_request_id },
+      (err, list_request) => {
+        if (err) {
+          res.status(404).send(err);
+        }
+        if (!list_request) {
+          res.json({
+            ok: false,
+            msg: 'Not found',
+          });
+        } else {
+          // Annoying js delete bug made me do this..
+          const {
+            company_id, company_image, company_email, company_name, company_location, password: listPassword, allow_remote, skills, title, exp, salary, details, how_to_apply,
+          } = list_request;
+          const saveList = new Lists({
+            company_id, company_image, company_email, company_name, company_location, password: listPassword, allow_remote, skills, title, exp, salary, details, how_to_apply,
+          });
+          saveList.save((err1, saved) => {
+            if (err1) {
+              res.status(500).send({
+                ok: false,
+                msg: err1,
+              });
+            } else {
+              res.json({
+                ok: true,
+                msg: 'Done approve request !',
+                data: { list_id: saved._id },
+              });
+              ListRequests.update(
+                { _id: list_request_id },
+                {
+                  $set: {
+                    is_approved: false,
+                    list_id: saved._id,
+                  },
+                }
+              );
+            }
+          });
+        }
+      }
+    );
+  }
+}
+
+/**
+ * Approve editing List request
+ */
+export function approveEditListRequest(req, res) {
   const { list_request_id } = req.params;
   const { password } = req.body;
   if (password !== tempPassword) {
--- server/models/ListRequests.js
+++ server/models/ListRequests.js
@@ -3,6 +3,11 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const requestSchema = new Schema({
+  request_type: {
+    type: String,
+    enum: ['new', 'edit'],
+    required: true,
+  },
   is_approved: { type: Boolean, default: false },
   list_id: { type: String, default: '' },
   additional_note: { type: String },
@@ -36,7 +41,7 @@ const requestSchema = new Schema({
     min: { type: Number, default: 0 },
     max: { type: Number, default: 9999999 },
   },
-  details: { type: String, required: true },
+  details: { type: Object, required: true },
   how_to_apply: { type: String, required: true },
 });
 
