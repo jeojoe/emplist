666125197b651d2de5d35782392e0b7c85dd2cd0
Jirat Onaree <jirat.onaree@gmail.com>
2017-01-12 12:52:52 +0700

    Add token expiration period

--- server/config.js
+++ server/config.js
@@ -2,6 +2,7 @@ const config = {
   mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/emplist',
   port: process.env.PORT || 8000,
   autoIndex: process.env.NODE_ENV !== 'production',
+  tokenExpires: '1h',
 };
 
 export default config;
