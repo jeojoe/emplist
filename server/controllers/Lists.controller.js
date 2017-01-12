da4d386ff34bf4ede110f571bd770991584e5e6c
Jirat Onaree <jirat.onaree@gmail.com>
2017-01-12 23:51:36 +0700

    Fix detail format in List controller

--- server/controllers/Lists.controller.js
+++ server/controllers/Lists.controller.js
@@ -1,6 +1,5 @@
 import Lists from '../models/Lists';
 import ListRequests from '../models/ListRequests';
-import Companies from '../models/Companies';
 import { signToken } from '../util/jwt-helpers';
 import bcrypt from 'bcrypt';
 // import Companies from '../models/Companies';
@@ -51,80 +50,6 @@ export function getListDetail(req, res) {
 }
 
 /**
- * Edit one list
- */
-export function updateList(req, res) {
-  const { id, title, tags, exp_condition, exp_between_min, exp_between_max, exp_more_than, intern_check, salary_min, salary_max, how_to_apply, company_name, company_image, remote_check, details, country, city, location_detail, company_id } = req.body.list;
-
-  if (!title || !tags || !company_name || !country || !city || !location_detail) {
-    res.status(403).end();
-  }
-
-  Companies.update(
-    { _id: company_id },
-    {
-      $set: {
-        company_image, company_name,
-        allow_remote: remote_check,
-        'company_location.country': country,
-        'company_location.city': city,
-        'company_location.detail': location_detail,
-        updated_at: Date.now,
-      },
-    },
-    (err) => {
-      if (err) res.status(500).send(err);
-      else {
-        let min = 0;
-        let max = 0;
-        if (exp_condition === 'between') {
-          min = exp_between_min;
-          max = exp_between_max;
-        } else if (exp_condition === 'more_than') {
-          min = exp_more_than;
-          max = 99;
-        } else {
-          min = 0;
-          max = 99;
-        }
-
-        const skills = tags.map((skill) => skill.text);
-        Lists.update(
-          { _id: id },
-          {
-            $set: {
-              company_name, title, details, how_to_apply,
-              'salary.min': salary_min || 0,
-              'salary.max': salary_max || 9999999,
-              'exp.condition': exp_condition,
-              'exp.has_intern': intern_check,
-              'exp.min': min,
-              'exp.max': max,
-              skills,
-              allow_remote: remote_check,
-              company_image,
-              'company_location.country': country,
-              'company_location.city': city,
-              'company_location.detail': location_detail,
-              updated_at: Date.now,
-            },
-          },
-          (err1) => {
-            if (err1) res.status(500).end(err);
-            else {
-              res.json({
-                ok: true,
-                msg: 'Done editing !',
-              });
-            }
-          }
-        );
-      }
-    }
-  );
-}
-
-/**
  * Edit one list (pagination)
  */
 export function sendEditListRequest(req, res) {
@@ -161,7 +86,7 @@ export function sendEditListRequest(req, res) {
         min: salary_min || 0,
         max: salary_max || 9999999,
       },
-      details,
+      details: JSON.stringify(details),
       how_to_apply,
       additional_note,
     });
@@ -212,7 +137,7 @@ export function checkPermission(req, res) {
         // Success -> create jwt then send it to client
         const token = signToken(list_id);
         res.json({
-          ok: true, msg: 'logged in yo', token,
+          ok: true, msg: 'Authentication\'s success', token,
         });
       }
     });
