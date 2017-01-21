a35cf28be3280704bc197b4cc5c6540d1b4ce59c
Jirat Onaree <jirat.onaree@gmail.com>
2017-01-21 23:06:55 +0700

    Change main font to Kanit

--- server/server.js
+++ server/server.js
@@ -91,7 +91,7 @@ const renderFullPage = (html, initialState) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="google-site-verification" content="2vjAlFV_HN6QWysSom046BOF4zyvr2bl0zaL3QWWeCs" />
         ${process.env.NODE_ENV === 'production' ? `<link rel='stylesheet' href='${assetsManifest['/app.css']}' />` : ''}
-        <link href='https://fonts.googleapis.com/css?family=Raleway:400,600,700' rel='stylesheet' type='text/css'/>
+        <link href="https://fonts.googleapis.com/css?family=Kanit" rel="stylesheet">
       </head>
       <body>
         <div id="root">${html}</div>
