bb109c7b3654c6b2b1dedd34236823145e4807c9
Jirat Onaree <jirat.onaree@gmail.com>
2017-02-02 14:22:59 +0700

    Add localized text

--- Intl/localizationData/en.js
+++ Intl/localizationData/en.js
@@ -2,5 +2,8 @@ export default {
   locale: 'en',
   messages: {
     headerDescription: '** Now Tech Jobs in Thailand only ** (Singapore’s coming soon!) Finding jobs shouldn’t be that hard. Let’s crowed sourcing them ! Employer can own a list describing all of its job . The post will be checked before being displayed and will be display for one month',
+    requestListHeader: 'Company can post a jobs for free. The post will be check and stay on site for one month. For promote listing please contact hi.emplist@gmail.com',
+    doneRequestDesc1: 'Sending request done ! we will let you know via your email if your list passes checking.',
+    doneRequestDesc2: 'Posting list is complete free. but it has a problem of ถูกดันลงไป. So we\'d like to know the needs of this feature. If you\'d like to have this feature on your list please click the button below..',
   },
 };
