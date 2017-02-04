067edb790e32ead5602209ddd7990de1a45dbb45
Jirat Onaree <jirat.onaree@gmail.com>
2017-02-04 15:22:20 +0700

    Create Intl in Request List Page

--- Intl/localizationData/en.js
+++ Intl/localizationData/en.js
@@ -1,10 +1,23 @@
+/*
+  rlp = RequestListPage
+*/
 export default {
   locale: 'en',
   messages: {
     headerDescription: 'All (tech) companies in one place. Find out about their culture, their work environment and what jobs they’re opening right now. ** Now only tech jobs in Thailand **',
     requestListButton: 'Request your company’s list',
     requestListHeader: 'Let everyone know your company and why they should work with you. One company can have one emplist for free. The list you request will be checked before being displayed on the site for 30 days. contact hi.emplist@gmail.com for making your list display again after expired or reporting problems.',
+    rlp_titleDesc: 'Use your creativity freely to create company’s emplist title ! (up to 120 chars.)',
+    rlp_skillsDesc: 'All skills required for opening positions. e.g. Javascript, PHP (at least 1, up to 7 skills)',
+    rlp_expDesc: 'experience range required for opening positions.',
+    rlp_salaryDesc: 'Optional, but preferred.',
+    rlp_detailsDesc: 'Introduce your company, its culture and work environment. Why does it exist. All jobs available. What you will offer, etc. (feel free to be creative with our editor !)',
+    rlp_howToApplyDesc: 'E.g. send us your resume to this email, go to company’s jobs site or company’s workable link.',
+    rlp_locationDetailDesc: 'E.g. near BTS Foo, in Bar Building.',
+    rlp_emailDesc: 'In case we need to contact you !',
+    rlp_passwordDesc: 'For managing your emplist in the next 30 days.',
     doneRequestDesc1: 'Sending request done ! we will let you know via your email if your list passes checking.',
     doneRequestDesc2: 'Posting list is complete free. but it has a problem of ถูกดันลงไป. So we\'d like to know the needs of this feature. If you\'d like to have this feature on your list please click the button below..',
+
   },
 };
