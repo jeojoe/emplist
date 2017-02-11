75d4240c2348e79e129a9edd9f93cc427a0afe18
Jirat Onaree <jirat.onaree@gmail.com>
2017-02-11 12:48:12 +0700

    Add Loader

--- client/modules/App/components/Loader.js
+++ client/modules/App/components/Loader.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import s from './Loader.css';
+import c from 'classnames';
 
-const Loader = () => (
-  <div className={s.loader}></div>
+const Loader = ({ black, size = '40px', borderWidth = '5px' }) => (
+  <div
+    className={c(s.loader, { [`${s.black}`]: black })}
+    style={{
+      width: size, height: size,
+      borderWidth, borderTopWidth: borderWidth,
+    }}
+  ></div>
 );
+
+Loader.propTypes = {
+  black: React.PropTypes.bool,
+  size: React.PropTypes.string,
+  borderWidth: React.PropTypes.string,
+};
 
 export default Loader;
